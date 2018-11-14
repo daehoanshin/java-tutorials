bbc5eb38cac792be1ef78bcd2e58f21ef450b758
DaeHoan Shin <xbb123@naver.com>
2018-11-14 14:17:36 +0900

    기본 테스트

--- src/org/opentutorials/javatutorials/numberstring/CharString.java
+++ src/org/opentutorials/javatutorials/numberstring/CharString.java
@@ -1,10 +1,10 @@
 package org.opentutorials.javatutorials.numberstring;
 
-public class Number {
+public class CharString {
 
 	public static void main(String[] args) {
-		System.out.println(1+2);
-		System.out.println(1.2+1.3);
+		System.out.println('생');
+		System.out.println("생활코딩");
 	}
 
 }
